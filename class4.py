19c45a367cbcf4baceba2037b95a9f8f9be7eb52
Mafuzur Rahaman <74731835+Mafuzur22@users.noreply.github.com>
2024-07-12 12:46:38 +0600

    Add files via upload

---
+++ class4.py
@@ -0,0 +1,96 @@
+# list_1 = [2, 3, 1, 4, 5]
+# print()
+
+
+# list_a = [100, 500, 200, 600, 800, 1100]
+# list_a.sort()
+# list_a.pop()
+# list_a.pop()
+# print(list_a)
+
+
+# tuple_1 = ("Hello", "World", "!")
+# list_b = list(tuple_1)
+# list_b.append("From Python")
+# tuple_1 = tuple(list_b)
+# print(tuple_1)
+# print(type(tuple_1))
+
+
+# tuple_1 = ("Hello", "World", "!")
+# tuple_2 = ("From Python",)
+# tuple_3 = tuple_1+tuple_2
+# print(tuple_3)
+
+
+# dict_1 = {1: "Sam",
+#           2: "Arnold",
+#           3: "Antony"
+#           }
+# dict_1.update({4: "William"})
+# dict_1.pop(2)
+# print(dict_1)
+
+
+# dict_2 = {
+#     'subdict_1' : {
+#         "name" : "Sahriar",
+#         "age" : 21
+        
+#     },
+#     'subdict_2' : {
+#         "name" : "Antony",
+#         "age" : 20
+#     },
+#     'subdict_3' : {
+#         "name" : "Mahafuzur",
+#         "age" : 21
+#     }
+    
+# }
+
+# dict_3 = dict_2['subdict_1']["age"],dict_2['subdict_2']["age"],dict_2['subdict_3']["age"]
+# print(dict_3)
+
+
+# dict_2 = {
+#     'subdict_1' : {
+#         "name" : "Sahriar",
+#         "age" : 21
+        
+#     },
+#     'subdict_2' : {
+#         "name" : "Antony",
+#         "age" : 20
+#     },
+#     'subdict_3' : {
+#         "name" : "Mahafuzur",
+#         "age" : 21
+#     }
+    
+# }
+
+# new_dict = dict_2["subdict_2"].copy()
+# print(new_dict)
+
+
+# list_3 =[1, 2, 3, 4, 566, 755, 5, 6, 7, 89, 9]
+
+# if 89 in list_3:
+#     print("89 In List")
+    
+# else:
+#     print("89 Not in list")
+
+
+# user_num = input("Please Enter a Number: ")
+# user_num = int(user_num)
+
+# if (user_num % 2 == 0):
+#     print(str(user_num)+" is Even")
+# else:
+#     print(str(user_num)+" is Odd")
+
+
+
+
